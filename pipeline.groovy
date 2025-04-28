4e0807363f3efb5b920e27745fe5cc109dadb9a2
shubham karale <152395655+Shub0705@users.noreply.github.com>
2025-04-28 15:17:22 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages{
         stage('code-pull'){
             steps {
-                git branch: 'dev', url: 'https://github.com/mayurmwagh/Project-frontend.git'
+                git branch: 'dev', url: 'https://github.com/Shub0705/Project-frontend.git'
             }
         }
         stage('code-build'){
@@ -16,9 +16,9 @@ pipeline {
         }
         stage('code-deploy'){
             steps {
-                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) 
                   sh '''
-                      aws s3 cp --recursive dist/angular-frontend s3://cbz-frontend-project-bux123-king/
+                      aws s3 cp --recursive dist/angular-frontend s3://cbz-frontend-project-bux123-king1/
                  '''
 }
                 
